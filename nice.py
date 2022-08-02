3e024cec9b95c3764de97c542c4e9e7edd9cf1ed
Joshua Alipao <joshuaalipao123@gmail.com>
2022-08-02 12:34:05 +0700

    nice.py is added

---
+++ nice.py
@@ -0,0 +1,9 @@
+age=23
+number=int(1)
+print(age +number ,'1st year age of james')
+age=23
+number=int(2)
+print(age +number ,'2nd year age of james')
+age=23
+number=int(3)
+print(age +number ,'3rd year age of james')
